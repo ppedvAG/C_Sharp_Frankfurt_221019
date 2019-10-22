557a0bc4fa2aa1ad1aa84c89bfe6f8fe24bc1db3
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2019-10-22 10:44:00 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            int alter;
+
+            alter = 31;
+
+            string stadt = "Berlin";
+
+            Console.WriteLine(alter);
+            Console.WriteLine(stadt);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
