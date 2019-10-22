1cc2b8f186747b7d715e4c5ee089ac7fb126f5fd
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2019-10-22 15:43:03 +0200

    Modul2: Kontrollstuckturen

---
+++ Kontrollstrukturen/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrollstrukturen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 56;
+            int b = 26;
+
+            if (a < b)
+            {
+                Console.WriteLine("A ist kleiner B");
+            }
+            else if (a > b)
+            {
+                Console.WriteLine("A ist größer als B");
+            }
+            else
+            {
+                Console.WriteLine("A ist gleich B");
+            }
+
+            //Kurznotation:
+            string ergebnis = (a == b) ? "A ist gleich B" : "A ist ungleich B";
+
+            while (a > b)
+            {
+                Console.WriteLine($"{a} ist größer als {b}");
+                a -= 10;
+
+                if (a == 36) break;
+            }
+
+            a = 120;
+
+            do
+            {
+                Console.WriteLine($"{a} ist kleiner als 100");
+                a *= 2;
+
+                continue;
+
+                Console.WriteLine("Wird niemals ausgeführt");
+
+            } while (a < 100);
+
+
+            for (int i = 0; i < 100; i += 2)
+            {
+                Console.WriteLine($"Dies ist der {i}. Durchgang.");
+            }
+
+            int[] zahlenarray = { 1, 789, 12, 45, 36, 78, 1245, 2, 0, 12 };
+
+            Console.WriteLine(zahlenarray[3]);
+            zahlenarray[8] = 123;
+
+            for (int i = 0; i < zahlenarray.Length; i++)
+            {
+                Console.WriteLine(zahlenarray[i]);
+            }
+
+            foreach (int item in zahlenarray)
+            {
+                Console.WriteLine(item);
+            }
+
+            int[,] zweiDimArray = new int[10, 10];
+
+            for (int i = 0; i < zweiDimArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < zweiDimArray.GetLength(1); j++)
+                {
+                    zweiDimArray[i, j] = i * j;
+                }
+            }
+            
+            Console.WriteLine(zweiDimArray[4,8]);
+
+
+            string beispiel = "Hallo";
+
+            Console.WriteLine(beispiel[2]);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Zufallszahlenuebung/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zufallszahlenuebung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int benutzerZahl;
+                Random generator;
+                int zufallsZahl;
+
+                generator = new Random();
+                zufallsZahl = generator.Next(1, 6);
+
+                do
+                {
+
+                    Console.Write("Gib eine Zahl zwischen 1 und 5 ein: ");
+                    benutzerZahl = int.Parse(Console.ReadLine());
+
+
+                    if (zufallsZahl < benutzerZahl)
+                    {
+                        Console.WriteLine("Deine Zahl ist zu groß.");
+                    }
+                    else if (zufallsZahl > benutzerZahl)
+                    {
+                        Console.WriteLine("Deine Zahl ist zu klein.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du hast richtig getippt.");
+                    }
+
+                } while (zufallsZahl != benutzerZahl);
+
+                Console.WriteLine("Möchtest du noch einmal spielen? (Y/N)");
+            } while (Console.ReadKey().Key == ConsoleKey.Y);
+        }
+    }
+}
